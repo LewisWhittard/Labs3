116e2d4e63a6982f744304312a092017be6b1e75
LewisWhittard <63914200+LewisWhittard@users.noreply.github.com>
2024-11-06 14:55:50 +0000

    Add project files.

---
+++ ConsoleApp1/Logic/LunchQue.cs
@@ -0,0 +1,49 @@
+﻿namespace ConsoleApp1.Logic
+{
+    public class LunchQue
+    {
+        private List<string> _lunchItems { get; set; }
+        private readonly Parser _parser = new Parser();
+
+        public LunchQue()
+        {
+            setLunchItems();
+        }
+
+        private void setLunchItems()
+        {
+            _lunchItems = new List<string>();
+            _lunchItems.Add("Fish");
+            _lunchItems.Add("Burgers");
+            _lunchItems.Add("Veg");
+        }
+
+        public string ConstuctMeal()
+        {
+            string Food = AskWhatWouldYouLike();
+            int Potatos = PotatoAmmount();
+            int Sprouts = SproutAmmount();
+            return $"Your lunch {Food} and {Potatos} Potatos and {Sprouts} sprouts";
+        }
+
+        public string AskWhatWouldYouLike()
+        {
+            Console.WriteLine($"What would you like {string.Join(", ",_lunchItems)}");
+            string value = Console.ReadLine();
+            return value;
+        }
+
+        public int PotatoAmmount()
+        {
+            string value = Console.ReadLine();
+            return _parser.GetIntFromString(value);
+        }
+
+        public int SproutAmmount()
+        {
+            string value = Console.ReadLine();
+            return _parser.GetIntFromString(value);
+        }
+    }
+}
+
---
+++ ConsoleApp1/Logic/Parser.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Logic
+{
+    public class Parser
+    {
+        public int GetIntFromString(string prompt)
+        {
+            try
+            {
+                return Convert.ToInt32(prompt);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+        }
+
+        public string GetIntFromString(int prompt)
+        {
+            try
+            {
+                return Convert.ToString(prompt);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+        }
+
+    }
+}
